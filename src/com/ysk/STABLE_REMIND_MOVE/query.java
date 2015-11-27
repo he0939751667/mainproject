fc047768ccc20ff4c5d49c5c6cc1bf0f20a034aa
B0052 <B0052@H00900-405.hq.ysp>
2015-11-27 15:56:50 +0800

    SECOND COMMIT FOR NEW 4Q_INSTRUMENT

--- src/com/ysk/STABLE_REMIND_MOVE/query.java
+++ src/com/ysk/STABLE_REMIND_MOVE/query.java
@@ -8,6 +8,7 @@ import setFuntion._hproc;
 
 public class query extends _hproc{
 	String tablename = "STABLEREMIND_MOVE"; //穩定性取樣提醒異動單
+	String projectname = "穩定性取樣提醒異動單";
 	public String action(String value)throws Throwable{
 		// 可自定HTML版本各欄位的預設值與按鈕的動作 
 		// 傳入值 value
@@ -16,7 +17,7 @@ public class query extends _hproc{
 		String MDATE = getValue("SDATE");
 		String MDATE1 = getValue("EDATE");
 		//queryFuntion 我自己把常用到的程式碼放到自訂的地方以便之後只要把參數代進去即可
-		String querydata[][] = queryFuntion(tablename,EMPID, PNO, MDATE, MDATE1);
+		String querydata[][] = queryFuntion(tablename,projectname,EMPID, PNO, MDATE, MDATE1);
 		setTableData("table1", querydata);
 		 return value;
 	}
